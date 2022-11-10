6f76bc8f3cc5d01e5fd403f4d029f036abb044e2
luckyrat <luckyrat@musites.com>
2022-11-10 09:55:22 +0000

    wip

---
+++ ios/KdbxSwift/Sources/KdbxSwift/util/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chris Tomlinson on 10/11/2022.
+//
+
+import Foundation
---
+++ ios/KeeVaultAutofill/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  KeeVaultAutofill
+//
+//  Created by Chris Tomlinson on 10/11/2022.
+//
+
+import Foundation
