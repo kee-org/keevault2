811cbd659de7e62a09d87fa3ae69d61d0dd42126
luckyrat <luckyrat@musites.com>
2022-11-10 16:12:11 +0000

    wip

--- ios/KdbxSwift/Sources/KdbxSwift/db/util/Cloneable.swift
+++ ios/KdbxSwift/Sources/KdbxSwift/db/util/Cloneable.swift
@@ -6,7 +6,7 @@ public protocol Cloneable {
 }
 
 public extension Array where Element == UInt8 {
-    public func clone() -> Array<UInt8> {
+    func clone() -> Array<UInt8> {
         return self.withUnsafeBufferPointer {
             [UInt8].init($0)
         }
